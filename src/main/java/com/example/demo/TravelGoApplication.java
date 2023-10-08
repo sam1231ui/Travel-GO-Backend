799cce24aca9ca14e27b3c0e7615ccb56d80b4e9
sam1231 <samruddhakumbhar@gmail.com>
2023-10-08 12:51:12 +0530

    all-Cross-origin

--- src/main/java/com/example/demo/TravelGoApplication.java
+++ src/main/java/com/example/demo/TravelGoApplication.java
@@ -2,6 +2,10 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TravelGoApplication {
@@ -10,6 +14,18 @@ public class TravelGoApplication {
 		SpringApplication.run(TravelGoApplication.class, args);
 		
 		System.out.println("Spring STARTED");
+	}
+	
+	@Bean
+	public WebMvcConfigurer configure() {
+		
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry reg) {
+				reg.addMapping("/**").allowedOrigins("*");
+			}
+			
+		};
 	}
 
 }
